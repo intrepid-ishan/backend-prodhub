44487769bc143408751387f6a79fc2fbcc851dc7
Ishan Makadia <coderishan@gmail.com>
2020-10-03 09:18:54 +0530

    basic validation

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,7 +1,14 @@
-const User = require('../models/user.js');
+const User                 = require('../models/user.js');
+const { validationResult } = require('express-validator');
 
 
 exports.signup = (req,res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()[0].msg });
+    }
+
     const user = new User(req.body);
     user.save((err, user) => {
         if(err){
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const router  = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const { check } = require('express-validator');
 
+//import controllers
 const { signup } = require('../controllers/auth'); 
 
-router.post('/signup',signup);
+router.post('/signup',[
+    check('name','Name should be atleast 5 char').isLength({min: 5}),
+    check('email','email is required').isEmail(),
+    check('password','Password should be atleast 5char').isLength({min:5})  
+],signup);
 
 
 module.exports = router;
